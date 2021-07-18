753028f09aea761d32600d40ddb21c524028cb11
Scap <picwin12@gmail.com>
2021-07-18 20:39:52 -0300

    fcomit

---
+++ src/components/Users.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div>
+      <h1>students</h1>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'Users',
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+
+</style>
